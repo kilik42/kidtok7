6a7cf611321e5a7c3c7c9c3e4d85c1c6eb6af212
kilik42 <marvinevins@yahoo.com>
2023-02-25 21:03:59 -0600

    working on app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="app">
- 
+      <h1>lets build a tik tok clone</h1>
+
+      {/* app container */}
+        {/* videos */}
+        
+      
     </div>
   );
 }
