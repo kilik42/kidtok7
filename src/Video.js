4c6ac318b95407195164b6fccb7c642a4432e798
kilik42 <marvinevins@yahoo.com>
2023-02-25 22:34:39 -0600

    working on playing pausing the video

--- src/Video.js
+++ src/Video.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React ,{useRef, useState} from 'react'
 import './Video.css'
 import ReactPlayer from 'react-player'
 function Video() {
+    const [playing, setPlaying] = React.useState(false);
+    const videoref = React.useRef(null);
+
+    const onVideoPress = () => {
+        if (playing) {
+            videoref.current.pause();
+        } else {
+            videoref.current.play();
+        }
+        setPlaying(!playing);
+    };
   return (
     <div className='video'>
      <ReactPlayer
@@ -9,6 +20,11 @@ function Video() {
         url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
         width='100%'
         height='100%'
+        loop
+        // controls
+        // controls={false}
+        onClick={onVideoPress}
+        ref = {videoref}
         />
      {/* videofooter */}
         {/* video sidebar */}
