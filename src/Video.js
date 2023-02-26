4d341ca6d92993f55658163decb3a3b8038922e2
kilik42 <marvinevins@yahoo.com>
2023-02-25 22:08:46 -0600

    video is working

--- src/Video.js
+++ src/Video.js
@@ -4,7 +4,12 @@ import ReactPlayer from 'react-player'
 function Video() {
   return (
     <div className='video'>
-     
+     <ReactPlayer
+        className='video__player'
+        url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+        width='100%'
+        height='100%'
+        />
      {/* videofooter */}
         {/* video sidebar */}
 
