b205e17ba6c657501d989e5b93b827712c3540c5
kilik42 <marvinevins@yahoo.com>
2023-02-26 23:04:41 -0600

    working on the ticker

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -11,10 +11,12 @@ function VideoFooter() {
       <div className='videoFooter__text'>
         <h3>@username</h3>
         <p>This is a description</p>
+        <div className="videoFooter__ticker">
         <MusicNoteIcon />
         {/* <Ticker mode='smooth'> 
         
         </Ticker> */}
+        </div>
       </div>
       <img className='videoFooterImg' src="https://thenounproject.com/api/private/icons/2052802/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABj_DYR_06tOHYMs2SF_Z_EWgPG2ydZk_ko5u9a7yfxtyyIDHK-8ep5l2sE5m5CJChyQGw7RRF_9vIm0co-ycv5nbq-sg%3D%3D" alt="" />
     </div>
