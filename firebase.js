1d6ede3d1ffa1decd9e6d77b041c8352a7b4b7cf
kilik42 <marvinevins@yahoo.com>
2023-02-27 21:23:04 -0600

    working on firebase

--- firebase.js
+++ firebase.js
@@ -1,12 +1,20 @@
-
+import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBycUG5BCK4sx4PTcoUqlMV99W84JcZ_CE",
-    authDomain: "tik-tok-clone-53a0e.firebaseapp.com",
-    projectId: "tik-tok-clone-53a0e",
-    storageBucket: "tik-tok-clone-53a0e.appspot.com",
-    messagingSenderId: "786766192588",
-    appId: "1:786766192588:web:a86f964ba1ec6bc8c48769",
-    measurementId: "G-E3XPVPMTF6"
+  apiKey: "AIzaSyArJ7i_CWk5PVLhikeczgxybOSPBHu-meQ",
+  authDomain: "tiktok4-fb41a.firebaseapp.com",
+  projectId: "tiktok4-fb41a",
+  storageBucket: "tiktok4-fb41a.appspot.com",
+  messagingSenderId: "110354040281",
+  appId: "1:110354040281:web:1f815e673fa1c9137f85c8",
+  measurementId: "G-WJFQ4HDXJP"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  export default db;
+
+// Path: kid_tok_4\src\Video.js
